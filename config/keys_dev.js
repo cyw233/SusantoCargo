608831d643e85fd33f98fbecd401e7687294bdb2
Chenyang Wang <chenyangw1@student.unimelb.edu.au>
2018-10-05 10:44:29 +1000

    add keys_dev.js to gitingonore

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-// this file has been "gitignore"
-module.exports = {
-  mongoURI: 'mongodb://colin:WANGcy888@ds127105.mlab.com:27105/spm_assgn2',
-  secretOrKey: 'secret',
-  emailPassword: 'WANGcy888'
-};
